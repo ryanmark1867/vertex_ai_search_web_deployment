e197f5712260eb123dae8dd7ab16666511eee415
ryanmark1867 <ryanmark2014@gmail.com>
2024-01-01 03:18:21 +0000

    remove extraneous imports

--- flask_web_deploy.py
+++ flask_web_deploy.py
@@ -1,22 +1,9 @@
-# example of using Flask to deploy a Keras deep learning model trained on a tabular dataset
+# example of using Flask to serve a simple web page
 import json
 import os
 import urllib.request
-import numpy as np
-import numpy as np
-
-
-# carry over imports from custom layer version
-
-
-# DSX code to import uploaded documents
 from io import StringIO
 import requests
-import json
-
-import os
-import yaml
-import math
 from flask import Flask, render_template, request
 
 
